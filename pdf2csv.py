279befabd03a15b418912f47fef7102f8a895515
radomax <rado.max.k@gmail.com>
2024-12-01 20:28:24 +0000

    pdf2csv.py modification

--- pdf2csv.py
+++ pdf2csv.py
@@ -1,60 +1,22 @@
-import sys
-from pdf2image import convert_from_path
 import pandas as pd
-import pytesseract
-import argparse
 
-def pdf_to_images(pdf_path, first_page=None, last_page=None, password=None):
-    """
-    Convert PDF file to images.
-    """
+def text_to_csv(input_path, output_path):
     try:
-        options = {
-            'dpi': 200,
-            'poppler_path': '/usr/bin'
-        }
-        if first_page:
-            options['first_page'] = first_page
-        if last_page:
-            options['last_page'] = last_page
-        if password:
-            options['password'] = password
-
-        return convert_from_path(pdf_path, **options)
+        # Read the text file into a DataFrame
+        df = pd.read_csv(input_path, delimiter=',', header=None)
+        
+        # Write the DataFrame to a CSV file
+        df.to_csv(output_path, index=False, header=False)
+        
+        print(f"Data successfully written to {output_path}")
     except Exception as e:
-        print(f"Error converting PDF to images: {e}")
-        sys.exit(1)
-
-def images_to_text(images):
-    """
-    Extract text from a list of image objects using Tesseract.
-    """
-    text_output = []
-    for image in images:
-        text = pytesseract.image_to_string(image)
-        text_output.append(text)
-    return text_output
-
-def text_to_csv(text_list, output_file):
-    """
-    Convert list of text to a CSV file.
-    """
-    df = pd.DataFrame(text_list, columns=['ExtractedText'])
-    df.to_csv(output_file, index=False)
-    print(f"Data extracted to {output_file}")
-
-def main():
-    parser = argparse.ArgumentParser(description="Convert PDF to CSV")
-    parser.add_argument('pdf_path', type=str, help='Path to the PDF file')
-    parser.add_argument('--firstpage', type=int, help='First page to convert', default=None)
-    parser.add_argument('--lastpage', type=int, help='Last page to convert', default=None)
-    parser.add_argument('--password', type=str, help='Password for the PDF file (optional)', default=None)
-    args = parser.parse_args()
-
-    images = pdf_to_images(args.pdf_path, args.firstpage, args.lastpage, args.password)
-    text_list = images_to_text(images)
-    text_to_csv(text_list, '/home/rado/Nordstjernen/rest_fp_241130.pdf')
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    main()
+    # Specify the input and output file paths
+    input_file_path = 'PATH.pdf'
+    output_file_path = 'PATH.csv'
+    
+    # Convert text file to CSV
+    text_to_csv(input_file_path, output_file_path)
 
